9ab928ca98cc335e79d39945713e69101386200c
RazvanZapodeanu <razvanzap@yahoo.com>
2025-11-08 16:53:02 +0200

    Dashboard

--- AmongClass/Controllers/TeacherController.cs
+++ AmongClass/Controllers/TeacherController.cs
@@ -1,6 +1,20 @@
-﻿namespace AmongClass.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AmongClass.Controllers
 {
-    public class TeacherController
+    public class TeacherController : Controller
     {
+        private readonly IRepository _repository; // sau contextul tău de date
+
+        public TeacherController(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IActionResult Dashboard()
+        {
+            var sessions = _repository.GetAllSessions(); // adaptează la metoda ta
+            return View(sessions);
+        }
     }
 }
