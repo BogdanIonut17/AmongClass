5b80a8e989f78556b23274da09d7bc5c6d67776f
BogdanIonut17 <BogdanIonut17@users.noreply.github.com>
2025-11-08 16:32:33 +0200

    CRUD Questions

---
+++ AmongClass/Controllers/SessionsController.cs
@@ -0,0 +1,19 @@
+﻿using AmongClass.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AmongClass.Controllers
+{
+    public class SessionsController(ApplicationDbContext context) : Controller
+    {
+        private readonly ApplicationDbContext db = context;
+        public IActionResult Index()
+        {
+            var sessions = db.Sessions.Include(s => s.Scores).Include(c => c.Teacher);
+
+            ViewBag.Sessions = sessions;
+
+            return View();
+        }
+    }
+}
